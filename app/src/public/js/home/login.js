5dd889736fcb9badc97c8ea5fe6f2d09619b2509
Moon-jbin <mjb1209@naver.com>
2021-12-24 00:05:54 +0900

    돔을 이용하여 html 객체 저장

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,2 +1,16 @@
-console.log("Hello World");
-console.log("bye");
+"use strict";
+
+const id = document.querySelector("#id"),
+  psword = document.querySelector("#psword"),
+  loginBtn = document.querySelector("button");
+
+loginBtn.addEventListener("click", login);
+
+function login() {
+  const req = {
+    id: id.value,
+    psword: psword.value,
+  };
+
+  console.log(req);
+}
